fedc75ce891a944947f4b784458a112734e55ba5
yuridido <yuridido@yahoo.it>
2020-07-23 17:01:51 +0200

    prove preimpazzimento

--- main.js
+++ main.js
@@ -8,6 +8,23 @@ $(document).ready(function() {
         indietro();
     });
 
+    $('.nav i').click(function() {
+        $('.nav i').removeClass('active');
+        $(this).addClass('active');
+        var trovato = false;
+        var i = 0;
+        while (trovato == false) {
+            if ($('.images').eq(i).hasClass('active')){
+                trovato = true;
+            } else {
+                i++;
+            }
+        }
+        trovato = false;
+        console.log(i);
+        $('.images .img').removeClass('active');
+        $('.images .img').eq(i).addClass('active');
+    });
 
 
 
@@ -51,6 +68,33 @@ $(document).ready(function() {
             tondoAttivo.prev().addClass('active');
         }
     };
+    // function avanti() {
+    //     var fotoAttiva = $('.images .active');
+    //     var tondoAttivo = $('.nav .active');
+    //     fotoAttiva.removeClass('active');
+    //     tondoAttivo.removeClass('active');
+    //     if (fotoAttiva.hasClass('last')) {
+    //         $('.images .first').addClass('active');
+    //         $('.nav .first').addClass('active');
+    //     } else {
+    //         fotoAttiva.next().addClass('active');
+    //         tondoAttivo.next().addClass('active');
+    //     }
+    // };
+    //
+    // function indietro() {
+    //     var fotoAttiva = $('.images .active');
+    //     var tondoAttivo = $('.nav .active');
+    //     fotoAttiva.removeClass('active');
+    //     tondoAttivo.removeClass('active');
+    //     if (fotoAttiva.hasClass('first')) {
+    //         $('.images .last').addClass('active');
+    //         $('.nav .last').addClass('active');
+    //     } else {
+    //         fotoAttiva.prev().addClass('active');
+    //         tondoAttivo.prev().addClass('active');
+    //     }
+    // };
 
 
 
