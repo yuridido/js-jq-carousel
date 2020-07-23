8382131c3c3c18ace86e2348aaf8b18a61b9eaa9
yuridido <yuridido@yahoo.it>
2020-07-23 17:14:54 +0200

    ripulito

--- main.js
+++ main.js
@@ -22,23 +22,9 @@ $(document).ready(function() {
             }
         }
         trovato = false;
-        console.log(i);
         $('.images img').removeClass('active');
         $('.images img').eq(i).addClass('active');
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     // FUNZIONI
@@ -69,34 +55,6 @@ $(document).ready(function() {
             tondoAttivo.prev().addClass('active');
         }
     };
-    // function avanti() {
-    //     var fotoAttiva = $('.images .active');
-    //     var tondoAttivo = $('.nav .active');
-    //     fotoAttiva.removeClass('active');
-    //     tondoAttivo.removeClass('active');
-    //     if (fotoAttiva.hasClass('last')) {
-    //         $('.images .first').addClass('active');
-    //         $('.nav .first').addClass('active');
-    //     } else {
-    //         fotoAttiva.next().addClass('active');
-    //         tondoAttivo.next().addClass('active');
-    //     }
-    // };
-    //
-    // function indietro() {
-    //     var fotoAttiva = $('.images .active');
-    //     var tondoAttivo = $('.nav .active');
-    //     fotoAttiva.removeClass('active');
-    //     tondoAttivo.removeClass('active');
-    //     if (fotoAttiva.hasClass('first')) {
-    //         $('.images .last').addClass('active');
-    //         $('.nav .last').addClass('active');
-    //     } else {
-    //         fotoAttiva.prev().addClass('active');
-    //         tondoAttivo.prev().addClass('active');
-    //     }
-    // };
-
 
 
 
